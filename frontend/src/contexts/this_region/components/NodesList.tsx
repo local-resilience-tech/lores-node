39185b6c516cbbcfc38992912f80834c642f3f0f
jade <jade@hopepunk.me>
2025-06-04 10:58:51 +1000

    nodes list is cards

--- frontend/src/contexts/this_region/components/NodesList.tsx
+++ frontend/src/contexts/this_region/components/NodesList.tsx
@@ -1,4 +1,4 @@
-import { Link, Table } from "@chakra-ui/react"
+import { Link, VStack, Card, Text, DataList } from "@chakra-ui/react"
 import { NodeDetailsWithStatus } from "../../this_node"
 
 const IpLink = ({ ip }: { ip: string }) => {
@@ -15,29 +15,35 @@ export default function NodesList({
   nodes: NodeDetailsWithStatus[]
 }) {
   return (
-    <Table.Root variant="line">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeader>Name</Table.ColumnHeader>
-          <Table.ColumnHeader>Node ID</Table.ColumnHeader>
-          <Table.ColumnHeader>IP</Table.ColumnHeader>
-          <Table.ColumnHeader>Status text</Table.ColumnHeader>
-          <Table.ColumnHeader>State</Table.ColumnHeader>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {nodes.map((node) => (
-          <Table.Row key={node.id}>
-            <Table.Cell>{node.name}</Table.Cell>
-            <Table.Cell>{node.id}</Table.Cell>
-            <Table.Cell>
-              {node.public_ipv4 && <IpLink ip={node.public_ipv4} />}
-            </Table.Cell>
-            <Table.Cell>{node.status_text}</Table.Cell>
-            <Table.Cell>{node.state}</Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+    <VStack alignItems="stretch" gap={4}>
+      {nodes.map((node) => (
+        <Card.Root>
+          <Card.Body gap="2">
+            <Card.Title mt="2">{node.name}</Card.Title>
+            <Text fontSize="xs" fontFamily="mono" mt={-2}>
+              {node.id}
+            </Text>
+            <DataList.Root orientation="horizontal">
+              <DataList.Item key="ip">
+                <DataList.ItemLabel>Message</DataList.ItemLabel>
+                <DataList.ItemValue>{node.status_text}</DataList.ItemValue>
+              </DataList.Item>
+              <DataList.Item key="ip">
+                <DataList.ItemLabel>IP</DataList.ItemLabel>
+                <DataList.ItemValue>
+                  {node.public_ipv4 || "unknown"}
+                </DataList.ItemValue>
+              </DataList.Item>
+              <DataList.Item key="state">
+                <DataList.ItemLabel>State</DataList.ItemLabel>
+                <DataList.ItemValue>
+                  {node.state || "unknown"}
+                </DataList.ItemValue>
+              </DataList.Item>
+            </DataList.Root>
+          </Card.Body>
+        </Card.Root>
+      ))}
+    </VStack>
   )
 }
