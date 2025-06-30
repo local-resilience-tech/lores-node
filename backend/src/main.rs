6901a2a2250a10659fd4b2580d6ebd88f0106ed2
jade <jade@hopepunk.me>
2025-06-30 21:10:56 +1000

    gst

---
+++ backend/src/main.rs
@@ -0,0 +1,39 @@
+use tower_http::cors::{self, CorsLayer};
+use utoipa::OpenApi;
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_swagger_ui::SwaggerUi;
+
+use crate::api::api_router;
+
+mod api;
+
+#[tokio::main]
+async fn main() {
+    #[derive(OpenApi)]
+    #[openapi()]
+    struct ApiDoc;
+
+    // CORS
+    let cors = CorsLayer::new()
+        .allow_origin(cors::Any)
+        .allow_headers(cors::Any);
+
+    // ROUTES
+    let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
+        .nest("/api", OpenApiRouter::new().merge(api_router()))
+        .split_for_parts();
+
+    let router = router
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api.clone()))
+        .layer(cors);
+
+    // SERVICE
+    let app = router.into_make_service();
+
+    // run our app with hyper, listening globally on port 8000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
+    println!("Listening on http://localhost:8000, Ctrl+C to stop");
+
+    axum::serve(listener, app).await.unwrap();
+}
