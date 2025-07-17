a798d268c43d0d0f4466d679d3f7cb964613a0e4
jade <jade@hopepunk.me>
2025-07-17 15:33:55 +1000

    new node form converted

--- frontend/src/contexts/this_node/components/NewNode.tsx
+++ frontend/src/contexts/this_node/components/NewNode.tsx
@@ -1,7 +1,5 @@
-import { Text, Input, VStack } from "@chakra-ui/react"
-import { useForm } from "react-hook-form"
-
-import { Field, Button, FormActions } from "../../../components"
+import { Stack, Text, TextInput, Button } from "@mantine/core"
+import { useForm } from "@mantine/form"
 
 export interface NewNodeData {
   name: string
@@ -14,16 +12,26 @@ export default function NewNode({
 }: {
   onSubmitNewNode: SubmitNewNodeFunc
 }) {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<NewNodeData>()
+  const form = useForm<NewNodeData>({
+    mode: "controlled",
+    initialValues: {
+      name: "",
+    },
+    validate: {
+      name: (value) => {
+        if (!value) return "This is required"
+        if (value.length > 50) return "Must be less than 50 characters"
+        if (!/^[a-z]+(-[a-z]+)*$/.test(value))
+          return "Lowercase letters only, no spaces, hyphens allowed"
+        return null
+      },
+    },
+  })
 
   return (
-    <VStack alignItems={"stretch"} width="100%">
-      <VStack alignItems={"flex-start"} textStyle={"xl"} gap={4}>
-        <Text>
+    <Stack>
+      <Stack>
+        <Text size="xl" fw={500}>
           Welcome to your new Node - part of a local, resilient, internet.
         </Text>
         <Text>To get setup, you'll need to choose a node name.</Text>
@@ -31,38 +39,22 @@ export default function NewNode({
           Ideally this should be unique within your region, but don't worry,
           you'll have a chance to change it later.
         </Text>
-      </VStack>
+      </Stack>
 
-      <form onSubmit={handleSubmit(onSubmitNewNode)}>
-        <VStack alignItems={"flex-start"} pt={8}>
-          <Field
+      <form onSubmit={form.onSubmit(onSubmitNewNode)}>
+        <Stack>
+          <TextInput
             label="Node Name"
-            helperText={`A name to identify your Node - use lowercase letters and no spaces`}
-            invalid={!!errors.name}
-            errorText={errors.name?.message}
-          >
-            <Input
-              {...register("name", {
-                required: "This is required",
-                maxLength: {
-                  value: 50,
-                  message: "Must be less than 50 characters",
-                },
-                pattern: {
-                  value: /^[a-z]+(-[a-z]+)*$/,
-                  message: "Lowercase letters only, no spaces, hyphens allowed",
-                },
-              })}
-            />
-          </Field>
-
-          <FormActions>
-            <Button loading={isSubmitting} type="submit">
-              Set Name
-            </Button>
-          </FormActions>
-        </VStack>
+            placeholder="Enter node name"
+            description="A name to identify your Node - use lowercase letters and no spaces"
+            key="name"
+            {...form.getInputProps("name")}
+          />
+          <Button loading={form.submitting} type="submit">
+            Set Name
+          </Button>
+        </Stack>
       </form>
-    </VStack>
+    </Stack>
   )
 }
