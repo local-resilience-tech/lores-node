1bed1c1925036637bdd7d862876d2a8c5b3d2e1d
jade <jade@hopepunk.me>
2025-07-22 13:06:08 +1000

    load this node at the start

--- frontend/src/store/loaders.ts
+++ frontend/src/store/loaders.ts
@@ -4,30 +4,38 @@ import { App, NodeDetails, Region } from "../api/Api"
 import { appsLoaded } from "./apps"
 import { nodesLoaded } from "./nodes"
 import { regionLoaded } from "./region"
+import { thisNodeLoaded } from "./this_node"
 
 export async function loadInitialData(store: AppStore) {
   const state = store.getState()
   if (state.region === null) loadRegion(store)
   if (state.nodes === null) loadNodes(store)
   if (state.apps === null) loadApps(store)
+  if (state.thisNode === null) loadThisNode(store)
 }
 
 async function loadRegion(store: AppStore) {
-  const result = await getRegion()
+  const result = await fetchApiData(() => getApi().api.showRegion())
   console.log("EFFECT: fetchRegion", result)
   if (result) store.dispatch(regionLoaded(result))
 }
 
 async function loadNodes(store: AppStore) {
-  const result = await getNodes()
+  const result = await fetchApiData(() => getApi().api.showAllNodes())
   console.log("EFFECT: fetchNodes", result)
   if (result) store.dispatch(nodesLoaded(result))
 }
 
 async function loadApps(store: AppStore) {
-  const result = await getApps()
+  const result = await fetchApiData(() => getApi().api.showAllApps())
   console.log("EFFECT: fetchApps", result)
   if (result) store.dispatch(appsLoaded(result))
+}
+
+async function loadThisNode(store: AppStore) {
+  const result = await fetchApiData(() => getApi().api.showThisNode())
+  console.log("EFFECT: fetchThisNode", result)
+  if (result) store.dispatch(thisNodeLoaded(result))
 }
 
 const fetchApiData = async <T>(
@@ -36,16 +44,4 @@ const fetchApiData = async <T>(
   const result = await apiCall()
   if (result.status >= 200 && result.status < 300) return result.data
   return null
-}
-
-const getRegion = async (): Promise<Region | null> => {
-  return fetchApiData(() => getApi().api.showRegion())
-}
-
-const getNodes = async (): Promise<NodeDetails[] | null> => {
-  return fetchApiData(() => getApi().api.showAllNodes())
-}
-
-const getApps = async (): Promise<App[] | null> => {
-  return fetchApiData(() => getApi().api.showAllApps())
 }
