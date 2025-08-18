d5891e86c7b504c3341ed2819e23472d940b165d
jade <jade@hopepunk.me>
2025-08-18 10:51:48 +1000

    removed local app actions from list page

--- frontend/src/contexts/apps/components/LocalAppsList.tsx
+++ frontend/src/contexts/apps/components/LocalAppsList.tsx
@@ -1,20 +1,7 @@
-import {
-  ActionIcon,
-  Button,
-  Group,
-  HoverCard,
-  Table,
-  Text,
-} from "@mantine/core"
-import {
-  AppDefinition,
-  AppRepo,
-  LocalApp,
-  LocalAppInstallStatus,
-} from "../../../api/Api"
-import { IconAlertCircle } from "@tabler/icons-react"
+import { Table } from "@mantine/core"
+import { AppDefinition, AppRepo, LocalApp } from "../../../api/Api"
 import { useLoading } from "../../shared"
-import { ActionResult, Anchor } from "../../../components"
+import { Anchor } from "../../../components"
 import LocalAppStatusBadge from "./LocalAppStatusBadge"
 
 export interface LocalAppWithRepo {
@@ -25,19 +12,9 @@ export interface LocalAppWithRepo {
 
 interface AppsListProps {
   apps: LocalAppWithRepo[]
-  appErrors?: Map<string, string>
-  onDeploy?: (app: LocalApp) => Promise<void>
-  onRemoveDeploy?: (app: LocalApp) => Promise<void>
-  onRegister?: (app: LocalApp) => Promise<void>
 }
 
-export default function LocalAppsList({
-  apps,
-  appErrors,
-  onDeploy,
-  onRemoveDeploy,
-  onRegister,
-}: AppsListProps) {
+export default function LocalAppsList({ apps }: AppsListProps) {
   return (
     <Table>
       <Table.Thead>
@@ -46,7 +23,6 @@ export default function LocalAppsList({
           <Table.Th>Version</Table.Th>
           <Table.Th>Latest</Table.Th>
           <Table.Th>Status</Table.Th>
-          <Table.Th>Actions</Table.Th>
         </Table.Tr>
       </Table.Thead>
       <Table.Tbody>
@@ -55,10 +31,6 @@ export default function LocalAppsList({
             key={appWithRepo.app.name}
             app={appWithRepo.app}
             repo_app_definition={appWithRepo.repo_app_definition}
-            onDeploy={onDeploy}
-            onRemoveDeploy={onRemoveDeploy}
-            onRegister={onRegister}
-            error={appErrors?.get(appWithRepo.app.name)}
           />
         ))}
       </Table.Tbody>
@@ -69,20 +41,9 @@ export default function LocalAppsList({
 interface LocalAppRowProps {
   app: LocalApp
   repo_app_definition?: AppDefinition
-  error?: string
-  onDeploy?: (app: LocalApp) => Promise<void>
-  onRemoveDeploy?: (app: LocalApp) => Promise<void>
-  onRegister?: (app: LocalApp) => Promise<void>
 }
 
-function LocalAppRow({
-  app,
-  repo_app_definition,
-  error,
-  onDeploy,
-  onRemoveDeploy,
-  onRegister,
-}: LocalAppRowProps) {
+function LocalAppRow({ app, repo_app_definition }: LocalAppRowProps) {
   const [loading, withLoading] = useLoading(false)
 
   const handleButtonPress = (
@@ -103,53 +64,6 @@ function LocalAppRow({
       <Table.Td>{repo_app_definition?.latest_version}</Table.Td>
       <Table.Td>
         <LocalAppStatusBadge status={app.status} />
-      </Table.Td>
-      <Table.Td>
-        <Group gap="xs">
-          {onDeploy && app.status === LocalAppInstallStatus.Installed && (
-            <Button
-              onClick={() => handleButtonPress(app, onDeploy)}
-              loading={loading}
-              size="xs"
-            >
-              Deploy
-            </Button>
-          )}
-          {onRemoveDeploy &&
-            app.status === LocalAppInstallStatus.StackDeployed && (
-              <Button
-                variant="outline"
-                color="red"
-                onClick={() => handleButtonPress(app, onRemoveDeploy)}
-                loading={loading}
-                size="xs"
-              >
-                Remove
-              </Button>
-            )}
-          {onRegister && app.status === LocalAppInstallStatus.StackDeployed && (
-            <Button
-              variant="outline"
-              onClick={() => handleButtonPress(app, onRegister)}
-              loading={loading}
-              size="xs"
-            >
-              Register
-            </Button>
-          )}
-          {error && (
-            <HoverCard width={280} shadow="md">
-              <HoverCard.Target>
-                <ActionIcon variant="transparent" color="red" size="lg">
-                  <IconAlertCircle />
-                </ActionIcon>
-              </HoverCard.Target>
-              <HoverCard.Dropdown>
-                <Text size="sm">{error}</Text>
-              </HoverCard.Dropdown>
-            </HoverCard>
-          )}
-        </Group>
       </Table.Td>
     </Table.Tr>
   )
--- frontend/src/contexts/apps/pages/LocalApps.tsx
+++ frontend/src/contexts/apps/pages/LocalApps.tsx
@@ -1,11 +1,8 @@
 import { ActionIcon, Container, Group, Stack, Title } from "@mantine/core"
 import { useAppSelector } from "../../../store"
 import LocalAppsList, { LocalAppWithRepo } from "../components/LocalAppsList"
-import { LocalApp } from "../../../api/Api"
-import { getApi } from "../../../api"
 import { IconPlus } from "@tabler/icons-react"
 import { useNavigate } from "react-router-dom"
-import { useState } from "react"
 
 type AppErrors = Map<string, string>
 
@@ -23,34 +20,6 @@ export default function LocalApps() {
     }) || []
 
   const navigate = useNavigate()
-  const [appErrors, setAppErrors] = useState<AppErrors>(new Map())
-
-  const handleError = (error: any, app: LocalApp) => {
-    const message = error.response?.data || "Unknown error"
-    setAppErrors((prev) => new Map(prev).set(app.name, message))
-    console.error("Error:", message)
-  }
-
-  const onAppDeploy = async (app: LocalApp) => {
-    console.log("Deploying app:", app)
-    getApi()
-      .api.deployLocalApp(app.name)
-      .catch((error) => handleError(error, app))
-  }
-
-  const onAppRemoveDeploy = async (app: LocalApp) => {
-    console.log("Deploying app:", app)
-    getApi()
-      .api.removeDeploymentOfLocalApp(app.name)
-      .catch((error) => handleError(error, app))
-  }
-
-  const onAppRegister = async (app: LocalApp) => {
-    console.log("Registering app:", app)
-    getApi()
-      .api.registerApp({ app_name: app.name })
-      .catch((error) => handleError(error, app))
-  }
 
   return (
     <Container>
@@ -62,15 +31,7 @@ export default function LocalApps() {
           </ActionIcon>
         </Group>
 
-        {apps && (
-          <LocalAppsList
-            apps={appsWithRepos}
-            onDeploy={onAppDeploy}
-            onRemoveDeploy={onAppRemoveDeploy}
-            onRegister={onAppRegister}
-            appErrors={appErrors}
-          />
-        )}
+        {apps && <LocalAppsList apps={appsWithRepos} />}
       </Stack>
     </Container>
   )
