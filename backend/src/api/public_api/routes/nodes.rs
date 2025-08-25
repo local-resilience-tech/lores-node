1c806074bb3aba71a4ee893aad79cfa17d402f2f
Jade <jade@hopepunk.me>
2025-08-25 13:38:42 +1000

    Local database (#13)
    * renamed main database and migrations to projections
    
    * fix github path
    
    * create shared db pools layer
    
    * removed duplication from db prep functions
    
    * create node data database

--- backend/src/api/public_api/routes/nodes.rs
+++ backend/src/api/public_api/routes/nodes.rs
@@ -1,8 +1,10 @@
 use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
-use sqlx::SqlitePool;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use crate::projections::{entities::NodeDetails, projections_read::nodes::NodesReadRepo};
+use crate::{
+    projections::{entities::NodeDetails, projections_read::nodes::NodesReadRepo},
+    DatabaseState,
+};
 
 pub fn router() -> OpenApiRouter {
     OpenApiRouter::new().routes(routes!(list_nodes))
@@ -12,10 +14,10 @@ pub fn router() -> OpenApiRouter {
     (status = 200, body = Vec<NodeDetails>),
     (status = INTERNAL_SERVER_ERROR, body = ()),
 ),)]
-async fn list_nodes(Extension(pool): Extension<SqlitePool>) -> impl IntoResponse {
+async fn list_nodes(Extension(db): Extension<DatabaseState>) -> impl IntoResponse {
     let repo = NodesReadRepo::init();
 
-    repo.all(&pool)
+    repo.all(&db.projections_pool)
         .await
         .map(|nodes| (StatusCode::OK, Json(nodes)))
         .map_err(|_| (StatusCode::INTERNAL_SERVER_ERROR, Json(())))
--- backend/src/infra/db.rs
+++ backend/src/infra/db.rs
@@ -1,10 +1,16 @@
+use anyhow::Result;
 use p2panda_store::sqlite::store::run_pending_migrations;
-use sqlx::{sqlite::SqliteConnectOptions, Pool, Sqlite, SqlitePool};
+use sqlx::{migrate::Migrator, sqlite::SqliteConnectOptions, Pool, Sqlite, SqlitePool};
 use std::env;
 
 lazy_static! {
     pub static ref DATABASE_URL: String =
-        env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:main.sqlite".to_string());
+        env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:projections.sqlite".to_string());
+}
+
+lazy_static! {
+    pub static ref NODE_DATA_DATABASE_URL: String = env::var("NODE_DATA_DATABASE_URL")
+        .unwrap_or_else(|_| "sqlite:node_data.sqlite".to_string());
 }
 
 lazy_static! {
@@ -12,44 +18,42 @@ lazy_static! {
         .unwrap_or_else(|_| "sqlite:operations.sqlite".to_string());
 }
 
-pub async fn prepare_main_database() -> anyhow::Result<Pool<Sqlite>> {
-    let filename = DATABASE_URL
-        .strip_prefix("sqlite:")
-        .ok_or_else(|| anyhow::anyhow!("DATABASE_URL must start with 'sqlite:'"))?;
+pub async fn prepare_projections_database() -> Result<Pool<Sqlite>> {
+    prepare_database(&DATABASE_URL, Some("./migrations_projectiondb")).await
+}
 
-    println!("Using database file: {}", filename);
+pub async fn prepare_operations_database() -> Result<Pool<Sqlite>> {
+    let pool = prepare_database(&OPERATION_DATABASE_URL, None).await?;
 
-    // create database if it does not exist
-    let options = SqliteConnectOptions::new()
-        .filename(filename)
-        .create_if_missing(true);
-
-    // prepare connection pool
-    let pool = SqlitePool::connect_with(options).await?;
-
-    // prepare schema in db if it does not yet exist
-    println!("Running migrations");
-    sqlx::migrate!().run(&pool).await?;
+    run_pending_migrations(&pool)
+        .await
+        .map_err(anyhow::Error::from)?;
 
     Ok(pool)
 }
 
-pub async fn prepare_operation_database() -> anyhow::Result<Pool<Sqlite>> {
-    let filename = OPERATION_DATABASE_URL
+pub async fn prepare_node_data_database() -> Result<Pool<Sqlite>> {
+    prepare_database(&NODE_DATA_DATABASE_URL, Some("./migrations_nodedatadb")).await
+}
+
+async fn prepare_database(db_url: &str, migrations: Option<&str>) -> Result<Pool<Sqlite>> {
+    let filename = db_url
         .strip_prefix("sqlite:")
-        .ok_or_else(|| anyhow::anyhow!("OPERATION_DATABASE_URL must start with 'sqlite:'"))?;
+        .ok_or_else(|| anyhow::anyhow!("Database URL must start with 'sqlite:'"))?;
 
-    println!("Using operation database file: {}", filename);
+    println!("Using database file: {}", filename);
 
-    // create database if it does not exist
     let options = SqliteConnectOptions::new()
         .filename(filename)
         .create_if_missing(true);
 
-    // prepare connection pool
     let pool = SqlitePool::connect_with(options).await?;
 
-    run_pending_migrations(&pool).await?;
+    if let Some(migrations_path) = migrations {
+        println!("Running migrations");
+        let migrator = Migrator::new(std::path::Path::new(migrations_path)).await?;
+        migrator.run(&pool).await?;
+    }
 
     Ok(pool)
 }
