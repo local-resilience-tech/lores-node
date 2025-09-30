23ed885dca64836317b88ff9f225ff4e2359c4e7
jade <jade@hopepunk.me>
2025-09-30 16:20:15 +1000

    clean up configure page

--- frontend/src/contexts/apps/pages/ConfigureApp.tsx
+++ frontend/src/contexts/apps/pages/ConfigureApp.tsx
@@ -1,3 +1,33 @@
+import { Breadcrumbs, Container, Stack, Title, Text } from "@mantine/core"
+import { Anchor } from "../../../components"
+import { useAppSelector } from "../../../store"
+import { useParams } from "react-router-dom"
+
 export default function ConfigureApp() {
-  return <div>Configure App Page - Under Construction</div>
+  const { appName } = useParams<{ appName: string }>()
+  const app = useAppSelector((state) =>
+    (state.localApps || []).find((a) => a.name === appName)
+  )
+
+  if (!appName || !app) {
+    return <Container>Error: App not found</Container>
+  }
+
+  return (
+    <Container>
+      <Stack gap="xs">
+        <Breadcrumbs>
+          <Anchor href="/this_node/apps">Local Apps</Anchor>
+          <Anchor href={`/this_node/apps/app/${app.name}`}>{app.name}</Anchor>
+          <Text c="dimmed">configure</Text>
+        </Breadcrumbs>
+        <Title order={1}>
+          <Text span inherit c="dimmed">
+            Configure App:{" "}
+          </Text>
+          {app.name}
+        </Title>
+      </Stack>
+    </Container>
+  )
 }
