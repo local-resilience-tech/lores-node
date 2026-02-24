e338ea9f4661708862596e5d0d90ee95a174f255
jade <jade@hopepunk.me>
2026-02-25 08:59:54 +1100

    collapsed migration history since we're starting again with the projection DB

--- backend/migrations_projectiondb/0001_init_schema.sql
+++ backend/migrations_projectiondb/0001_init_schema.sql
@@ -1,18 +1,46 @@
 -- Add migration script here
+CREATE TABLE regions (
+    id VARCHAR(36) PRIMARY KEY NOT NULL,
+    creator_node_id VARCHAR(36) NOT NULL,
+    slug TEXT NOT NULL,
+    name TEXT NOT NULL,
+    organisation_name TEXT NOT NULL,
+    url TEXT NOT NULL
+);
+
 CREATE TABLE nodes (
     id VARCHAR(36) PRIMARY KEY NOT NULL,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    public_ipv4 VARCHAR(15) DEFAULT NULL,
+    domain_on_local_network TEXT DEFAULT NULL,
+    domain_on_internet TEXT DEFAULT NULL
 );
 
-CREATE TABLE network_configs (
-    id INT PRIMARY KEY NOT NULL,
-    network_name VARCHAR(255),
-    bootstrap_node_id VARCHAR(64),
-    bootstrap_node_ip4 VARCHAR(15)
+CREATE TABLE node_statuses (
+    operation_id VARCHAR(64) PRIMARY KEY NOT NULL,
+    node_id VARCHAR(36) NOT NULL,
+    text VARCHAR(255) NULL,
+    state VARCHAR(50) NULL,
+    posted_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE node_configs (
-    id INT PRIMARY KEY NOT NULL,
-    public_key_hex VARCHAR(36),
-    private_key_hex VARCHAR(64)
+CREATE TABLE current_node_statuses (
+    node_id VARCHAR(36) PRIMARY KEY NOT NULL,
+    text VARCHAR(255) NULL,
+    state VARCHAR(50) NULL,
+    posted_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE apps (
+  name TEXT NOT NULL UNIQUE,
+  PRIMARY KEY (name)
+);
+
+CREATE TABLE app_installations (
+  app_name TEXT NOT NULL,
+  node_id TEXT NOT NULL,
+  version TEXT NOT NULL,
+  FOREIGN KEY (app_name) REFERENCES apps(name),
+  FOREIGN KEY (node_id) REFERENCES nodes(id),
+  PRIMARY KEY (app_name, node_id)
 );
