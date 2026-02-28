e1ed5a6dd9012a0fce23f5f7de5cec38c5b938b9
jade <jade@hopepunk.me>
2026-02-28 11:23:40 +1100

    loads nodes when creating region

--- backend/lores-node-axum/src/event_handlers/region_created.rs
+++ backend/lores-node-axum/src/event_handlers/region_created.rs
@@ -3,9 +3,9 @@ use sqlx::SqlitePool;
 use crate::{
     api::public_api::client_events::ClientEvent,
     data::{
-        entities::{Region, RegionWithNodes},
+        entities::{Region, RegionNodeStatus, RegionWithNodes},
         projections_read::region_nodes::RegionNodesReadRepo,
-        projections_write::regions::RegionsWriteRepo,
+        projections_write::{region_nodes::RegionNodesWriteRepo, regions::RegionsWriteRepo},
     },
     event_handlers::handler_utilities::{handle_db_write_error, HandlerResult},
     panda_comms::lores_events::{LoResEventHeader, RegionCreatedDataV1},
@@ -44,12 +44,15 @@ impl RegionCreatedHandler {
         payload: RegionCreatedDataV1,
         pool: &SqlitePool,
     ) -> Result<RegionWithNodes, sqlx::Error> {
-        let repo = RegionsWriteRepo::init();
+        let region_write_repo = RegionsWriteRepo::init();
+        let node_write_repo = RegionNodesWriteRepo::init();
         let node_read_repo = RegionNodesReadRepo::init();
+
+        let node_id = header.author_node_id;
 
         let region = Region {
             id: payload.region_id,
-            creator_node_id: Some(header.author_node_id),
+            creator_node_id: Some(node_id.clone()),
             slug: Some(payload.slug),
             name: Some(payload.name),
             organisation_name: payload.organisation_name,
@@ -58,8 +61,20 @@ impl RegionCreatedHandler {
             user_conduct_url: payload.user_conduct_url,
             user_privacy_url: payload.user_privacy_url,
         };
+        region_write_repo.upsert(pool, &region).await?;
 
-        repo.insert(pool, &region).await?;
+        // Upsert region node status
+        node_write_repo
+            .upsert_join_status(
+                pool,
+                &node_id,
+                &region.id,
+                RegionNodeStatus::Member,
+                None,
+                None,
+                None,
+            )
+            .await?;
 
         let result = node_read_repo.append_detailed_nodes(pool, &region).await?;
 
