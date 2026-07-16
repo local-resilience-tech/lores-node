cf5674c4f56fad5911f6ea4b9ef200d78665c933
jade <jade@hopepunk.me>
2026-07-16 21:59:38 +1000

    can forget a region

--- frontend/src/contexts/regions/pages/EnsureJoinedRegion.tsx
+++ frontend/src/contexts/regions/pages/EnsureJoinedRegion.tsx
@@ -1,15 +1,53 @@
-import { Container, Title, Text } from "@mantine/core"
+import { Container, Title, Text, Stack } from "@mantine/core"
 import { RegionNodeStatus } from "../../../api/Api"
 import { useAppSelector } from "../../../store"
 import { myActiveRegionNode } from "../../../store/my_regions"
 import { Outlet } from "react-router-dom"
+import { ActionButton, actionSuccess, actionFailure } from "../../../components"
+import { getApi } from "../../../api"
+import { IfNodeSteward } from "../../auth/node_steward_auth"
 
-function RegionNotJoined() {
+function RegionNotJoined({
+  region_id,
+}: {
+  region_id: string | null | undefined
+}) {
+  const onForgetRegion = () => {
+    if (!region_id) return Promise.reject(new Error("No region_id provided"))
+
+    return getApi()
+      .nodeStewardApi.forgetRegion({ region_id })
+      .then(() => actionSuccess())
+      .catch((error) => actionFailure(error))
+  }
+
   return (
-    <div style={{ padding: "2rem" }}>
-      You have not joined this region. Please join the region to view its
-      details.
-    </div>
+    <Container>
+      <Title order={1} mb="md">
+        Region not joined
+      </Title>
+      <Stack align="flex-start">
+        <Text>
+          You have not joined this region. Please join the region to view its
+          details.
+        </Text>
+        <IfNodeSteward>
+          {!region_id ? (
+            <Text color="red">
+              No region ID provided. You cannot forget this region.
+            </Text>
+          ) : (
+            <ActionButton
+              variant="outline"
+              color="red"
+              onClick={onForgetRegion}
+            >
+              Forget region
+            </ActionButton>
+          )}
+        </IfNodeSteward>
+      </Stack>
+    </Container>
   )
 }
 
@@ -25,6 +63,9 @@ function RequestPending() {
 }
 
 export default function EnsureJoinedRegion() {
+  const activeRegionId = useAppSelector(
+    (state) => state.my_regions.activeRegionId,
+  )
   const currentStatus = useAppSelector(
     (state) =>
       myActiveRegionNode(state.my_regions, state.network?.node.id)?.status ??
@@ -32,7 +73,7 @@ export default function EnsureJoinedRegion() {
   )
 
   if (currentStatus === null) {
-    return <RegionNotJoined />
+    return <RegionNotJoined region_id={activeRegionId} />
   }
 
   switch (currentStatus) {
